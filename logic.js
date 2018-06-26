bf21cf1136a396954f46d7c0f99355b58edc4067
Max Robb <maxbenrobb@maxs-mbp.attlocal.net>
2018-06-26 17:24:04 -0400

    full html and js variables

---
+++ logic.js
@@ -0,0 +1,20 @@
+//GLOBAL VARIABLES
+//============================================================
+//arrays and variables for data
+var wordOptions = ["apple", "banana", "grape", "strawberry", "orange"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+//game counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+//FUNCTIONS
+//============================================================
+
+//MAIN PROCESS
+//============================================================
