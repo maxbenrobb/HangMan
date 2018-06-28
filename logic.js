bc049f1efa109747f3f3f5e4849f21b210632e3e
Max Robb <maxbenrobb@maxs-mbp.attlocal.net>
2018-06-28 10:41:16 -0400

    html beginning to populate

--- logic.js
+++ logic.js
@@ -20,10 +20,28 @@ function startGame () {
     selectedWord =wordOptions[Math.floor(Math.random() * wordOptions.length)];
     lettersinWord = selectedWord.split("");
     numBlanks = lettersinWord.length;
+
+    // reset
+    guessesLeft = 9;
+    wrongLetters = [];
+    blanksAndSuccesses = [];
+
+    // populate blanks and successes with right number of blanks
+    for (var i=0; i<numBlanks; i++){
+        blanksAndSuccesses.push("_")
+    }
+
+    // change HTML
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+
     //test / debugging
     console.log(selectedWord);
     console.log(lettersinWord);
     console.log(numBlanks);
+    console.log(blanksAndSuccesses);
 }
 
 //MAIN PROCESS
